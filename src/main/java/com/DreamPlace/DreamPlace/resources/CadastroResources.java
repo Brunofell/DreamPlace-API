57243b35dc99956027572a4807a7d13b6abb6e6b
Brunofell <brunofelimarti@gmail.com>
2023-11-05 16:52:28 -0300

    Cadastro pronto

--- src/main/java/com/DreamPlace/DreamPlace/resources/CadastroResources.java
+++ src/main/java/com/DreamPlace/DreamPlace/resources/CadastroResources.java
@@ -3,10 +3,13 @@ package com.DreamPlace.DreamPlace.resources;
 import com.DreamPlace.DreamPlace.DTOs.CadastroDTO;
 import com.DreamPlace.DreamPlace.domain.Cadastro;
 import com.DreamPlace.DreamPlace.services.CadastroService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,6 +33,26 @@ public class CadastroResources {
         List<CadastroDTO> listDTO = list.stream().map(obj -> new CadastroDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
     }
+
+    @PostMapping
+    public ResponseEntity<Cadastro> create(@Valid @RequestBody Cadastro obj){
+        obj = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CadastroDTO> update(@Valid @PathVariable Integer id, @RequestBody CadastroDTO objDTO){
+        Cadastro newObj = service.update(id, objDTO);
+        return ResponseEntity.ok().body(new CadastroDTO(newObj));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 }
--- src/main/java/com/DreamPlace/DreamPlace/services/CadastroService.java
+++ src/main/java/com/DreamPlace/DreamPlace/services/CadastroService.java
@@ -1,6 +1,8 @@
 package com.DreamPlace.DreamPlace.services;
 
+import com.DreamPlace.DreamPlace.DTOs.CadastroDTO;
 import com.DreamPlace.DreamPlace.domain.Cadastro;
+import com.DreamPlace.DreamPlace.exceptions.DataIntegrityViolationException;
 import com.DreamPlace.DreamPlace.exceptions.ObjectNotFoundException;
 import com.DreamPlace.DreamPlace.repository.CadastroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +27,32 @@ public class CadastroService {
         return repository.findAll();
     }
 
+
+    public Cadastro create(Cadastro obj){
+        obj.setId(null);
+        return repository.save(obj);
+    }
+
+    public Cadastro update(Integer id, CadastroDTO objDTO) {
+        Cadastro obj = findById(id);
+        obj.setNome(objDTO.getNome());
+        obj.setEmail(objDTO.getEmail());
+        obj.setSenha(objDTO.getSenha());
+        obj.setNumero(objDTO.getNumero());
+        obj.setCpf(objDTO.getCpf());
+        return repository.save(obj);
+    }
+
+
+    public void delete(Integer id) {
+        findById(id);
+        try{
+            repository.deleteById(id);
+        }catch (org.springframework.dao.DataIntegrityViolationException e){
+            throw new DataIntegrityViolationException(
+                    "User não pode ser deletado! Possui reservas associadas.");
+        }
+
+    }
 
 }
