1c512855fe0b83bbd61d8f2acc8aebda71e97ce2
Brunofell <brunofelimarti@gmail.com>
2023-11-05 12:32:04 -0300

    Criei a API

---
+++ src/main/java/com/DreamPlace/DreamPlace/DreamPlaceBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.DreamPlace.DreamPlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamPlaceBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamPlaceBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DreamPlace/DreamPlace/DreamPlaceBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DreamPlace.DreamPlace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamPlaceBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
